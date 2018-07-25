eda84a41847affa6a2c0a6649594b5848f04042a
Harsh Jain <harjain@microsoft.com>
2018-07-25 15:52:24 +0530

    Added Coverage Coverage Utility to create xml file and print summray.

---
+++ src/Coverage.TestLogger.Tests/CodeCoverageUtilityTests.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+using System;
+
+namespace Microsoft.TestPlatform.Extensions.CoverageLogger.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.IO;
+    using System.Reflection;
+
+    [TestClass]
+    public class CodeCoverageUtilityTests
+    {
+        private CodeCoverageUtility codeCoverageUtility;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.codeCoverageUtility = new CodeCoverageUtility();
+        }
+
+        [TestMethod]
+        public void AnalyzeCoverageFileShouldGenerateXMLFile()
+        {
+            var testContainerDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            this.codeCoverageUtility.AnalyzeCoverageFile(Path.Combine(testContainerDirectory, @"TestData\Sample.coverage"), Path.Combine(testContainerDirectory, @"TestData\CodeCoverageExe\CodeCoverage"));
+
+            var expectedFileText = File.ReadAllText(Path.Combine(testContainerDirectory, @"TestData\Expected.xml"));
+            var generatedFileText = File.ReadAllText(Path.Combine(testContainerDirectory, @"TestData\Sample.xml"));
+
+            File.Delete(Path.Combine(testContainerDirectory, @"TestData\Sample.xml"));
+
+            Assert.AreEqual(expectedFileText, generatedFileText, "Generated file doesn't have correct data.");
+
+
+        }
+
+        [TestMethod]
+        public void GetCoverageXMLShouldReturnSummary()
+        {
+            var testContainerDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var xmlFilePath =Path.Combine(testContainerDirectory, @"TestData\Expected.xml");
+            var expectedSummary = Environment.NewLine +
+                                  "Code Coverage" + Environment.NewLine +
+                                  "----------------" + Environment.NewLine +
+                                  "Module Name\t\t\tNot Covered(Blocks)\t\tNot Covered(% Blocks)\t\tCovered(Blocks)\t\tCovered(% Blocks)" + Environment.NewLine +
+                                  "unittestproject7.dll\t\t1\t\t\t\t\t100\t\t\t\t0\t\t\t\t0.00" + Environment.NewLine;
+
+            var summary = this.codeCoverageUtility.GetCoverageSummary(xmlFilePath);
+
+            Assert.AreEqual(expectedSummary, summary, "Summy is not correct");
+        }
+    }
+}
---
+++ src/Coverage.TestLogger/CoverageLogger.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+namespace Microsoft.VisualStudio.TestPlatform.Extensions.CoverageLogger
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using Microsoft.TestPlatform.Extensions.CoverageLogger;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+    using Microsoft.VisualStudio.TestPlatform.Utilities;
+
+    /// <summary>
+    /// Logger for Generating CodeCoverage Analysis
+    /// </summary>
+    [FriendlyName(FriendlyName)]
+    [ExtensionUri(CoverageUri)]
+    internal class CoverageLogger : ITestLogger
+    {
+        #region Constants
+
+        private const string CoverageUri = "datacollector://microsoft/CodeCoverage/2.0";
+
+        private const string FriendlyName = "CoverageLogger";
+
+
+        private const string SetupInteropx86 = @"x86\Microsoft.VisualStudio.Setup.Configuration.Native.dll";
+
+        private const string SetupInteropx64 = @"x64\Microsoft.VisualStudio.Setup.Configuration.Native.dll";
+
+        private static readonly Uri CodeCoverageDataCollectorUri = new Uri(CoverageUri);
+
+        private ManualResetEvent coverageXmlGenerateEvent;
+
+        private CodeCoverageUtility codeCoverageUtility;
+
+        #endregion
+
+        #region ITestLogger
+
+        /// <inheritdoc/>
+        public void Initialize(TestLoggerEvents events, string testResultsDirPath)
+        {
+            System.Diagnostics.Debugger.Launch();
+            if (events == null)
+            {
+                throw new ArgumentNullException(nameof(events));
+            }
+
+            // Register for the events.
+            events.TestRunComplete += this.TestRunCompleteHandler;
+
+            this.codeCoverageUtility = new CodeCoverageUtility();
+        }
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Called when a test run is completed.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// Test run complete events arguments.
+        /// </param>
+        internal void TestRunCompleteHandler(object sender, TestRunCompleteEventArgs e)
+        {
+            if (e.AttachmentSets == null)
+            {
+                return;
+            }
+
+            var coverageAttachments = e.AttachmentSets
+                .Where(dataCollectionAttachment => CodeCoverageDataCollectorUri.Equals(dataCollectionAttachment.Uri)).ToArray();
+
+            if (coverageAttachments.Any())
+            {
+                var codeCoverageFiles = coverageAttachments.Select(coverageAttachment => coverageAttachment.Attachments[0].Uri.LocalPath).ToArray();
+
+                foreach (var codeCoverageFile in codeCoverageFiles)
+                {
+                    var resultFile = Path.Combine(Path.GetDirectoryName(codeCoverageFile), Path.GetFileNameWithoutExtension(codeCoverageFile) + ".xml");
+                    var arguments = "analyze /output:" + '"' + resultFile + '"' + " " + '"' + codeCoverageFile + '"';
+                    try
+                    {
+                        this.codeCoverageUtility.AnalyzeCoverageFile(arguments, this.GetCodeCoverageExePath());
+                    }
+                    catch (Exception ex)
+                    {
+                        ConsoleOutput.Instance.WriteLine(ex.Message, OutputLevel.Information);
+                    }
+
+                    var summary =  this.codeCoverageUtility.GetCoverageSummary(resultFile);
+                    ConsoleOutput.Instance.WriteLine(summary, OutputLevel.Information);
+                }
+            }
+        }
+
+        private string GetCodeCoverageExePath()
+        {
+            return @"C:\Users\harjain\.nuget\packages\microsoft.internal.codecoverage\1.0.9\contentFiles\any\any\CodeCoverage";
+        }
+
+        #endregion
+    }
+}
