52d904ca4cce5944e4561317e827da39e81cf166
Harsh Jain <harjain@microsoft.com>
2018-07-25 16:38:38 +0530

    PR feedback.

--- src/Coverage.TestLogger.Tests/CodeCoverageUtilityTests.cs
+++ src/Coverage.TestLogger.Tests/CodeCoverageUtilityTests.cs
@@ -13,24 +13,32 @@ namespace Microsoft.TestPlatform.Extensions.CoverageLogger.Tests
     [TestClass]
     public class CodeCoverageUtilityTests
     {
+        private string codeCoverageExePath;
+        private string sampleCodeCoverageFilePath;
+        private string expectedXMLFilePath;
+        private string sampleXMLFilePath;
         private CodeCoverageUtility codeCoverageUtility;
 
         [TestInitialize]
         public void Initialize()
         {
             this.codeCoverageUtility = new CodeCoverageUtility();
+            var testContainerDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            this.sampleCodeCoverageFilePath = Path.Combine(testContainerDirectory, @"TestData\Sample.coverage");
+            this.expectedXMLFilePath = Path.Combine(testContainerDirectory, @"TestData\Expected.xml");
+            this.sampleXMLFilePath = Path.Combine(testContainerDirectory, @"TestData\Sample.xml");
+            this.codeCoverageExePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\..\..\packages\microsoft.codecoverage\15.8.0\build\netstandard1.0\CodeCoverage");
         }
 
         [TestMethod]
         public void AnalyzeCoverageFileShouldGenerateXMLFile()
         {
-            var testContainerDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            this.codeCoverageUtility.AnalyzeCoverageFile(Path.Combine(testContainerDirectory, @"TestData\Sample.coverage"), Path.Combine(testContainerDirectory, @"TestData\CodeCoverageExe\CodeCoverage"));
+            this.codeCoverageUtility.AnalyzeCoverageFile(this.sampleCodeCoverageFilePath, this.codeCoverageExePath);
 
-            var expectedFileText = File.ReadAllText(Path.Combine(testContainerDirectory, @"TestData\Expected.xml"));
-            var generatedFileText = File.ReadAllText(Path.Combine(testContainerDirectory, @"TestData\Sample.xml"));
+            var expectedFileText = File.ReadAllText(this.expectedXMLFilePath);
+            var generatedFileText = File.ReadAllText(this.sampleXMLFilePath);
 
-            File.Delete(Path.Combine(testContainerDirectory, @"TestData\Sample.xml"));
+            File.Delete(this.sampleCodeCoverageFilePath);
 
             Assert.AreEqual(expectedFileText, generatedFileText, "Generated file doesn't have correct data.");
 
@@ -40,17 +48,15 @@ namespace Microsoft.TestPlatform.Extensions.CoverageLogger.Tests
         [TestMethod]
         public void GetCoverageXMLShouldReturnSummary()
         {
-            var testContainerDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var xmlFilePath =Path.Combine(testContainerDirectory, @"TestData\Expected.xml");
             var expectedSummary = Environment.NewLine +
                                   "Code Coverage" + Environment.NewLine +
                                   "----------------" + Environment.NewLine +
                                   "Module Name\t\t\tNot Covered(Blocks)\t\tNot Covered(% Blocks)\t\tCovered(Blocks)\t\tCovered(% Blocks)" + Environment.NewLine +
                                   "unittestproject7.dll\t\t1\t\t\t\t\t100\t\t\t\t0\t\t\t\t0.00" + Environment.NewLine;
 
-            var summary = this.codeCoverageUtility.GetCoverageSummary(xmlFilePath);
+            var summary = this.codeCoverageUtility.GetCoverageSummary(this.expectedXMLFilePath);
 
-            Assert.AreEqual(expectedSummary, summary, "Summy is not correct");
+            Assert.AreEqual(expectedSummary, summary, "Summary is not correct");
         }
     }
 }
